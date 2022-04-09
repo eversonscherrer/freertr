f16b4068d7f6a429aa4462759fd86d9a5a376f22
Everson <eversonborges@gmail.com>
2022-04-09 13:33:32 -0300

    segrout

---
+++ segrout/start-topology.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RTR=/Users/everson/Dropbox/rtr/rtr.jar
+HWSW=/Users/everson/Dropbox/rtr/everson/segrout/
+
+tmux new-session -d -s rare 'java -jar '$RTR' routersc '$HWSW'r1-hw.txt '$HWSW'r1-sw.txt'
+tmux split-window -v -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r2-hw.txt '$HWSW'r2-sw.txt' ENTER;
+tmux split-window -h -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r3-hw.txt '$HWSW'r3-sw.txt' ENTER;
+tmux split-window -h -t 2 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r4-hw.txt '$HWSW'r4-sw.txt' ENTER;
+tmux split-window -v -t 0 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r5-hw.txt '$HWSW'r5-sw.txt' ENTER;
+tmux split-window -v -t 2 -p 50
+tmux send 'java -jar '$RTR' routersc '$HWSW'r6-hw.txt '$HWSW'r6-sw.txt' ENTER;
+tmux select-layout tiled
+tmux a;
